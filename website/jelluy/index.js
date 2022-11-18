8c084b5d729ac65f6de7c064503e48b20982ca94
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-11-18 14:51:58 -0500

    Update index.js

--- website/jelluy/index.js
+++ website/jelluy/index.js
@@ -1,2 +1,32 @@
-var count = Math.random();
-jelluy().startSite(`<h1>Current State of count: ${count}</h1><button id="j" onclick="jelluy().renderPage('<h1>Hello ${count}</h1>', '<title>Hello</title>');jelluy().addLine('<p>This website is made in js no HTML check the source it 100% javascript(other then boilplate code to make library work)</p>','')">Done</button>`)
+function website() {
+ jelluy.renderPage(`<h1>Docs</h1>`,``)
+}
+function startWeb() {
+ jelluy.startSite(`
+ <main class="bg-blua">
+  <h1 class="text-3xl font-bold text-center text-red">
+    Welcome to jelluy docs
+  </h1>
+  <button class="bg-sky-500 hover:bg-sky-700" onclick="website()">Start making a website</button>
+ </main>
+ `, `
+ <script src="https://cdn.tailwindcss.com"></script>
+ <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            blua: '#155fa3',
+            red: '#ff0000',
+          },
+          backgrounds: {
+            blua: '#155fa3',
+          }
+        }
+      }
+    }
+ </script>
+ `); 
+}
+startWeb();

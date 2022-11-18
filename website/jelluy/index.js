abd2ab11c48af61618be1ddf6d2f907db7461632
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-11-17 21:33:35 -0500

    Create index.js

---
+++ website/jelluy/index.js
@@ -0,0 +1,2 @@
+var count = Math.random();
+jelluy().startSite(`<h1>Current State of count: ${count}</h1><button id="j" onclick="jelluy().renderPage('<h1>Hello ${count}</h1>', '<title>Hello</title>');jelluy().addLine('<p>This website is made in js no HTML check the source it 100% javascript(other then boilplate code to make library work)</p>','')">Done</button>`)

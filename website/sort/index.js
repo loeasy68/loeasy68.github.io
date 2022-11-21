fe73e1f5661bab5f59f955335a61b2b7c7a0e552
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-11-21 15:03:35 -0500

    Update index.js

--- website/sort/index.js
+++ website/sort/index.js
@@ -1,1 +1,26 @@
-
+var links = [
+"P",
+"F",
+"P",
+"T",
+"O",
+"P",
+"T",
+"T",
+"O",
+"O",
+"W",
+"G",
+"Y",
+"M",
+"M",
+"G",
+"D",
+"G",
+"S",
+"H",
+"C",
+"D"
+]
+links.sort()
+console.log(links)

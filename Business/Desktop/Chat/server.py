4da6df415e0824c53aa351c82931f7e9adbe8ed7
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-11-27 19:13:17 -0500

    Create server.py

---
+++ Business/Desktop/Chat/server.py
@@ -0,0 +1,52 @@
+import socket
+import threading
+
+HOST = "0.0.0.0"
+PORT = "80000"
+
+server = socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[client.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = client.index(client) 
+            clients.remove(client)     
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connent with {str(address)}!")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nickname.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} connented to  th server!\n".encode('utf-8'))
+        client.send("Connented to the server".endode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+        print("Server running...")
+        receive()

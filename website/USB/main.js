08b89deefc14acc1d5cec64dcc7d3956437c7d97
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-08 14:38:12 -0500

    Update main.js

--- website/USB/main.js
+++ website/USB/main.js
@@ -2,5 +2,7 @@ let os = prompt("What is your OS")
 let homeDir = ""
 if (os = "ChromeOS") {
  homeDir = "/"
+}
+function vidTube() {
  result.innerHTML = homeDir + "media/removable/Code/code/VidTube/index.html"
 }

d911dc16481f0b3f274641f61069a294acec91ad
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-09 18:57:05 -0500

    i

---
+++ website/website.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+spelling = $1
+if [ spelling == 1]
+then
+  echo "Coach"
+  echo "Exhausted"
+  echo "Release"
+  echo "Toilet"
+  echo "Youth"
+  echo "Highlight"
+  echo "Oatmeal"
+  echo "Remaining"
+  echo "Bonus words"
+  echo "Irritate"
+  echo "Revererend"
+  echo "Impatient"
+fi

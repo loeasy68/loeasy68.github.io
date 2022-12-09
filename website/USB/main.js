e57375946c1e79b59acde1a9f70e47bc402d80bc
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-09 17:37:02 -0500

    l

--- website/USB/main.js
+++ website/USB/main.js
@@ -1,10 +1,18 @@
+//Inits
 let os = prompt("What is your OS")
 let vidTubeURL = "/media/removable/Code/code/VidTube/index.html"
 let vidTubeMac = "/Volumes/Code/code/VidTube/index.html"
+let loGamesURL = "/media/removable/Code/code/LoGames/index.html"
+let loGamesMac = "/Volumes/Code/code/LoGames/index.html"
+
+// Check OS
 vidTubeURL = (os == "MacOS") ? vidTubeMac : vidTubeURL;
-if (os = "ChromeOS") {
- homeDir = "/"
-}
+loGames = (os == "MacOS") ? loGamesMac : loGamesURL;
+
+// Functioms
 function vidTube() {
  result.innerHTML = vidTubeURL
+}
+function loGames() {
+	result.innerHTML = loGamesURL
 }

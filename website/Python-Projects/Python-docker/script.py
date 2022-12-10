cee1e8fa16255469c5c668b41c9e12687b70cbde
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-10 14:29:06 -0500

    d

--- website/Python-Projects/Python-docker/script.py
+++ website/Python-Projects/Python-docker/script.py
@@ -1,2 +1,19 @@
-import flask
+from flask import Flask, redirect, url_for
 import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>LoGoat</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ = "__main__":
+    app.run()

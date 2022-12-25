192d20286f728dd6137b48539f78a2e056951703
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-25 18:07:34 +0000

    Fixed Docker

--- website/Docker/build.sh
+++ website/Docker/build.sh
@@ -1,2 +1,3 @@
+docker pull php:7.0-apache
 docker build -t hello-world .
-docker run -p 80:80 -v ./src/:/var/www/html/ hello-world
+docker run -p 80:80 -v /src/:/var/www/html/ hello-world
--- website/Docker/src/index.php
+++ website/Docker/src/index.php
@@ -1,4 +1,3 @@
 <?php
 
 echo "Hello, world<br><a href='https://hub.docker.com'>Docker hub</a>"
-

bffc250b60086dee3dcc40cd042f69ae36358d52
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-30 11:58:39 -0500

    j

---
+++ GAME-ENIGNE/index.js
@@ -0,0 +1,8 @@
+alert("Name of project:")
+jelluy().startSite(`
+<button>Export</button>
+`,`
+<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" /> 
+<script defer src="https://pyscript.net/latest/pyscript.js"></script>
+`)

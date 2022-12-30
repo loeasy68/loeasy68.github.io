9d8161ffc924a5eec801556889dbb86f391dc3c1
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2022-12-30 10:09:23 -0500

    Update index.js

--- Business/index.js
+++ Business/index.js
@@ -3,37 +3,35 @@ function prices() {
     <h1>Prices</h1>
     <ol>
       <li>Beginner-5 months access Plus Previews</li>
-      <li>
         <ul>
           <li>$5 per week</li>
           <li>$20 per month</li>
           <li>$240 per year</li>
         </ul>
-      </li>
       <li>Medium-10 months access Plus Editor</li>
-      <li>
         <ul>
           <li>$10 per week</li>
           <li>$40 per month</li>
           <li>$480 per year</li>
         </ul>
-      </li>
       <li>Serious-Unlimited months access Plus Previews and Editor</li>
-      <li>
         <ul>
           <li>$20 per week</li>
           <li>$80 per month</li>
           <li>$960 per year</li>
         </ul>
-      </li>
     </ol>
     `,`
     
     `)
 }
 jelluy().startSite(`
-<h1>Welcome LoganHTTPServices</h1>
-<button onclick="prices()">Prices</button>
+<h1 style="text-align: center">Welcome LoganHTTPServices</h1>
+
+<div>
+  <h3>Prices</h3>
+  <button onclick="prices()">Get the prices</button>
+</div>
 `,`
 
 `)

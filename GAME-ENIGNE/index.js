7f6d3f55e532c12f1032c4552fa4ab51d76514b2
Logan Jones <a.jones3145@gmail.com>
2022-12-31 08:44:32 -0500

    This adds to the game enigne

--- GAME-ENIGNE/index.js
+++ GAME-ENIGNE/index.js
@@ -1,7 +1,23 @@
 const name = prompt("Name of project:")
 function downloadGame() {
-  // Make a game file
-  console.log("Worked")  
+  let file = new File([`
+  <!DOCTYPE HTML>
+  <html>
+    <head>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" /> 
+    <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js" integrity="sha512-Qlv6VSKh1gDKGoJbnyA5RMXYcvnpIqhO++MhIM2fStMcGT9i2T//tSwYFlcyoRRDcDZ+TYHpH8azBBCyhpSeqw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    </head>
+    <body>
+      Not implemented
+    </body>
+  </html>
+  `], "game.html", {type: "text/plain;charset=utf-8"}) 
+  saveAs(file)
 }
 jelluy().startSite(`
 <div>

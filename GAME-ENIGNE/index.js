6963ae228fa38c2f7e6d3851eb9941b204a9a4e0
Logan Jones <a.jones3145@gmail.com>
2022-12-31 09:16:48 -0500

    This is a working change

--- GAME-ENIGNE/index.js
+++ GAME-ENIGNE/index.js
@@ -19,10 +19,20 @@ function downloadGame() {
   `], "game.html", {type: "text/plain;charset=utf-8"}) 
   saveAs(file)
 }
+function backgroundPage() {
+  jelluy().renderPage(`
+  <h1>Background Page</h1>
+  `, `
+  
+  `)
+}
 jelluy().startSite(`
 <div>
-<a id="exportGame" class="waves-effect waves-light btn" onclick="download()">Export ${name}</a>
+<a id="exportGame" class="waves-effect waves-light btn">Export ${name}</a>
+<a id="background" class="waves-effect waves-light btn">Set a background image</a>
+<div id="link"></div>
 </div>
+
 <div class="box"></div>
 `,`
 <link rel="stylesheet" href="styles.css">
@@ -31,9 +41,12 @@ jelluy().startSite(`
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js" integrity="sha512-Qlv6VSKh1gDKGoJbnyA5RMXYcvnpIqhO++MhIM2fStMcGT9i2T//tSwYFlcyoRRDcDZ+TYHpH8azBBCyhpSeqw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 `)
+background.addEventListener("click", function(e) {
+  e.preventDefault()
+  backgroundPage()
+})
 exportGame.addEventListener("click", function(e){
     e.preventDefault()
-    download()
+    downloadGame()
 })
